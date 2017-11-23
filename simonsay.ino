73545703fc6befe4f5d17cdbe88b5ff90b8ab5d6
arseniiyamnii <yamnii@ya.ru>
2017-11-23 17:31:43 +0500

    Это готово! Готово и работает!
    Остается только добавить redme и коменты расставить лучше!))))
    Юхуууу!

--- simonsay.ino
+++ simonsay.ino
@@ -1,83 +1,136 @@
-////////////////////////Все переменные/////////////////////////////////
-int button = 2;
+/*Тупостарт*/
+  int led1 = 8;
+  int button1 = 2;
+  int led2 = 9;
+  int button2 = 3;
 
-int led1 = 8;
-int button2 = 3;
-int led2 = 9;
-int(x);
-int(y);
-int(cifr);
-int arraycifr[10]={1,2,1,2,1,1,1,2,1};
-int (a);
-int(timecifr);
-int(b);
-int(roundd);
-int(c);
-int(k);
-int(pere);
-int(nround);
-int(rrr);
-///////////////////////(Объявление переверии)////////////////////////////
-void setup() {
+  void setup() {
 
-pinMode(led1, OUTPUT);
+    pinMode(led1, OUTPUT);
 
-pinMode(button, INPUT);
+    pinMode(button1, INPUT);
 
-pinMode(led2, OUTPUT);
+    pinMode(led2, OUTPUT);
 
-pinMode(button2, INPUT);
-}
-//////////////////////ф-я для переворота числа///////////////////////
-int perevernut(int c, int k){
-    while(c>0.1){
-    k=k*10+c%10;
-  c/=10;}
-  return(k);
-  }
-int rround(int b, int r){
-  while(b>0.1){
-    b/=10;
-    r=r+1;
-  }
-  return r;
-}
-int migan(int rrr){
-      if (rrr==1){
-              digitalWrite(led1, HIGH);
+    pinMode(button2, INPUT);
+    }
+/*Начинаются функции*/
+  void mig(int a) {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      if (a == 1) {
+        digitalWrite(led1, HIGH);
         delay(1000);
         digitalWrite(led1, LOW);
-            }
-            if (rrr==2)
-            {
-              digitalWrite(led2, HIGH);
+        delay(1000);
+      }
+      if (a == 2)
+      {
+        digitalWrite(led2, HIGH);
         delay(1000);
         digitalWrite(led2, LOW);
-                          }
-}
-/////////////////////Непосредственно программа////////////////////////
-int main (){
-  cifr=1212121212;
-  roundd=rround(cifr,0);
-  pere=perevernut(cifr,0);
-    nround=0;
-  while(roundd>0){   ////все раунды////
-       nround++;       ////счетчик номера раунда////
-       /////мигание///
-       while(nround>0){
-            rrr=pere%10;
-            migan(rrr);
-            pere/=10;
-       nround--;
-       }
-       /////сбор данных с кнопок////
-       /////проверка данных с кнопок////
-       
+        delay(1000);
+      }}
+  void fullmig(long a) {
+    int b;
+    while (a!=0) {
+      b = a % 10;
+      a = a/10;
+      mig(b);
+      
+    }}
+  int sborskn(){
+    int x=0;
+    while (x==0) {
+    if (digitalRead(button1) == HIGH) {
+    x=1;
+    while(digitalRead(button1) == HIGH){}}
+    if (digitalRead(button2) == HIGH) {
+    x=2;
+    while(digitalRead(button2) == HIGH){}
+    }}
+    delay (250);
+    return x;}
+  long soed(long a, int b){
+    long c;
+    long d = 0;
+    while(d == 0){
+    d=(long)b;}
+    c=a*10+d;
+    return c;}
+  long sbor(int a){
+    long c=0;
+    int b=0;
+    int x;
+    for (a;a>0;a--) {
+      x=0;
+      b=sborskn();
+      c=soed(c,b);
+      b=0;
+    }
 
-
-
-
-
-    roundd--;    ////отсчет раунда//////
-  }
-}
+      return c;
+    
+    }
+  int dlinc(long a){
+      int b=0;
+      while(a!=0){
+        b++;
+        a/=10;
+      }
+      return b;
+      }
+  long chislodc(int i,long c){
+    int b;
+    int a=0;
+    for (i; i!=0; i--)
+      {
+      a=a*10+c%10;
+      c/=10;
+      }
+    return a;
+    }
+  void (* resetFunc) (void) = 0;
+  void prov(long a, long b){
+    if(a==b){
+      for (int i = 5; i!=0 ;i--){
+        digitalWrite(led1, HIGH);
+        delay (250);
+        digitalWrite(led1, LOW);
+        delay (250);
+        }}
+        else{
+          for (int i = 5; i!=0 ;i--){
+        digitalWrite(led2, HIGH);
+        delay (250);
+        digitalWrite(led2, LOW);
+        delay (250);
+        }
+        resetFunc();
+        }
+        }
+  long pere(long a){
+    long b=0;
+    while (a!=0){
+      b=b*10+a%10;
+      a/=10;
+    }
+    return b;
+    }
+/*начинается код*/
+  void loop(){
+    long a=1212; /////Это число, которое мы используем!!!!
+    int e=0;
+    int c=dlinc(a); //количество раундов
+    long b=0;
+    long d=0;
+    for (c;c!=0;c--){ //остчет раундов
+      delay(2000);
+      ++e;
+      b=chislodc(e,a);
+      fullmig(b);
+      d=sbor(e);
+      d=pere(d);
+      prov(b,d);
+    }
+    }
