db00a85199aa96c81a9e89d09261687265345c1e
arseniiyamnii <yamnii@ya.ru>
2017-12-08 21:45:43 +0500

    q

--- othersketch/sswithcom.ino
+++ othersketch/sswithcom.ino
@@ -1,20 +1,103 @@
-const int ledPin = 8;
-const int ledPin2 = 9;
-int incomingByte;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-}
-
+/*
+проба сделать без функций и указателей
+ */
+/*
+Это вступление
+ */
+int led = 8;
+int button = 2;
+int ledtwo = 9;
+int buttontwo = 3;
+//testcommit1
+   void setup() {
+   pinMode(led, OUTPUT);
+   pinMode(button, INPUT);
+   pinMode(ledtwo, OUTPUT);
+   pinMode(buttontwo, INPUT);
+   Serial.begin(9600);
+   randomSeed(analogRead(0));
+    }
+void mig(int a) {
+      digitalWrite(led, LOW);
+      digitalWrite(ledtwo, LOW);
+      if (a == 1) {
+        digitalWrite(led, HIGH);
+        delay(1000);
+        digitalWrite(led, LOW);
+        delay(1000);
+      }
+      if (a == 2){
+        digitalWrite(ledtwo, HIGH);
+        delay(1000);
+        digitalWrite(ledtwo, LOW);
+        delay(1000);
+      }
+    }
+    void fullmig(int *a, int i){
+      int b=0;
+      int c=0;
+      int d=0;
+      while(b!=i){
+        c=a[b];
+        mig(c);
+       b++;
+      }
+    }
+    int sizearr(int *a){ //Этой функцией мы узнаем длинну массива
+      int d=0;
+      while(a[d] != 3){
+        d++;
+      }
+      return d;
+    }
+    /*
+    сбор с кнопок
+     */
+    int sborskn(){
+      int x=0;
+      while (x==0) {
+        if (digitalRead(button) == HIGH) {
+          x=1;
+          while(digitalRead(button) == HIGH){}
+        }
+        if (digitalRead(buttontwo) == HIGH) {
+          x=2;
+          while(digitalRead(buttontwo) == HIGH){}
+        }
+      }
+      delay (250);
+      return x;
+    }
+    /*
+    Проверка
+     */
+    void (* resetFunc) (void) = 0;
+    void prov(int a){
+        if(a==1){
+          for (int p = 5; p!=0 ;p--){
+            digitalWrite(led, HIGH);
+            delay (250);
+            digitalWrite(led, LOW);
+            delay (250);
+          }
+        }
+        else{
+          for (int p = 5; p!=0 ;p--){
+            digitalWrite(ledtwo, HIGH);
+            delay (250);
+            digitalWrite(ledtwo, LOW);
+            delay (250);
+          }
+         resetFunc();
+        }
+      }
 void loop() {
-int x=1000;
+int x=10000;
 while (millis()<x)
 {
   if (Serial.available() > 0) {
-  x=10000;
-    incomingByte = Serial.read();
+  x=100000;
+    int incomingByte = Serial.read();
     if (incomingByte == '1') {
       digitalWrite(ledPin, HIGH);
     }
@@ -28,29 +111,5 @@ while (millis()<x)
       digitalWrite(ledPin2, LOW);
     }
   }}
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
-  digitalWrite(ledPin2, HIGH);
-  delay(250);
-  digitalWrite(ledPin2, LOW);
-  delay(250);
   
 }
