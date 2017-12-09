9d446b262c2a18e0a40cf6f93c75fc1eb4d2065f
arseniiyamnii <yamnii@ya.ru>
2017-12-09 23:48:17 +0500

    кажется готово

--- simonsay.ino
+++ simonsay.ino
@@ -20,10 +20,10 @@ int buttontwo = 3;
 /*
 Реально нужные функции
  */
-    void mig(int a, int i) {
+    void mig(int a, int i, int speedp) {
       digitalWrite(led, LOW);
       digitalWrite(ledtwo, LOW);
-      int t=1000;
+      int t=speedp;
       if(t>200){
       	t=t-i*50;
       }
@@ -42,13 +42,13 @@ int buttontwo = 3;
         digitalWrite(ledtwo, LOW);
       }
     }
-    void fullmig(int *a, int i){
+    void fullmig(int *a, int i, int speedp){
       int b=0;
       int c=0;
       int d=0;
       while(b!=i){
         c=a[b];
-        mig(c,i);
+        mig(c,i,speedp);
        b++;
       }
     }
@@ -104,11 +104,106 @@ int buttontwo = 3;
          resetFunc();
         }
       }
+      int sborc(){
+        int inc = Serial.parseInt();
+        return inc;
+      }
     /*
     начало проги
      */
 void loop(){
-  int d=10;
+	//начало поднастройки программы
+	//в настройке должны менятся:
+	//1.кол-во раундов
+	//2.скорость промигивания(несколько вариантов)
+	//3.время между раундами(несколько вариантов)
+  int time=10000;
+  int rtime=0;
+  int kolr=11;
+  int speedp=1000;
+  int speedr=2000;
+  while (rtime<time){
+  	rtime=millis();
+  	if (Serial.available() > 0) {
+  		Serial.println("Добро пожаловать в интерфейс редактирования игры!");
+  		Serial.println("Веедите номер настройки, которую хотите изменить");
+  		Serial.println("1 - Изменение кол-ва раундов");
+  		Serial.println("2 - Изменение скорости промигивания лампочек");
+  		Serial.println("3 - Изменение промежутка между раундами");
+  		Serial.println("Жду команды");
+  		Serial.println("----------------------------");
+  		rtime=1;
+  		int a=sborc();
+  		int b=0;
+  		Serial.flush();
+  		if (a==1)//команда изменения раундов
+  		{
+  			Serial.println("Вы выбрали ИЗМЕНЕНИЕ КОЛ-ВА РАУНДОВ");
+  			Serial.println("Автоматически стоит настройка 10 раундов");
+  			Serial.println("Чтобы ее не менять, напиши 10");
+  			Serial.println("Просто напиши число раундов");
+  			Serial.println("Жду");
+  			Serial.println("-----------------------------");
+  			b=0;
+  			while(b==0){
+  			b=sborc();}
+  			kolr=b+1;
+  			Serial.println("теперь в игре будет ");
+  			Serial.print(b);
+  			Serial.println(" раундов");
+  		}
+  		if (a==2)//команда изменения скорости промигивания
+  		{
+  			Serial.println("Вы выбрали команду изменения скорости промигивания диодов");
+  			Serial.println("Сейчас режим с ускорением");
+  			Serial.println("Скорости начинается от 1с. и минимум который может быть,");
+  			Serial.println("это 0.25с.");
+  			Serial.println("Напишите свою скорость с которой начинать");
+  			b=0;
+  			while(b==0){
+  			b=sborc();}
+  			speedp=b;
+  			Serial.print("Теперь скорость будет ");
+  			Serial.print(speedp/1000);
+  			Serial.println(" секунд");
+  		}
+  		if (a==3)//команда изменения скорости раундов
+  		{
+  			Serial.println("Настройка промежутка между раундами");
+  			Serial.println("По умолчанию промежуток 2с");
+  			Serial.println("Напиши свой вариант");
+  			b=0;
+  			while(b==0){
+  			b=speedr();}
+  			speedr=b;
+  			Serial.print("Теперь промежуток равен ");
+  			Serial.print(speedr/1000);
+  			Serial.println(" секунд");
+  		}
+  		if (a==4)//команда изменения скорости раундов
+  		{
+  			Serial.println("Вы выбрали выход из настройки");
+  			Serial.println("Вы уверенны?");
+  			Serial.println("1=да");
+  			Serial.println("2=нет");
+  			b=0;
+  			while(b==0){
+  			b=speedr();}
+  			if (b==0)
+  			{
+  				Serial.println("Продолжение настройки");
+  			}
+  			if (b==1)
+  			{
+  				Serial.println("начинаем игру через 3 секунды");
+  				delay(3000);
+  				rtime=20000;
+  			}
+  		}
+
+
+  }
+  int d=kolr;
   int a[d];
   int cif=0;
   for (int q = 0; q < d; ++q)
@@ -120,7 +215,7 @@ void loop(){
   }
   int i=1;//тут начинается отсчет раундов
   while(i<d){
-    delay(2000);
+    delay(speedr);
     //собираем число для раунда
     int ch[i];
     for (int k = 0; k < i; ++k)
@@ -129,7 +224,7 @@ void loop(){
     }
     //промигаем это число
     int *chuk=ch;
-    fullmig(chuk,i);
+    fullmig(chuk,i,speedp);
     //начинаем сбор с кнопок
     int sb[i];
     for (int k = 0; k < i; ++k)
